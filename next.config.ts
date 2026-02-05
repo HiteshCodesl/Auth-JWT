8030808e2fce60019662a1e2345132b83030be9c
Hitesh Navare <navarehitesh65@gmail.com>
2026-02-05 17:50:42 +0530

    added

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig: NextConfig & { [key: string]: any } = {
+  devIndicators: false,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
